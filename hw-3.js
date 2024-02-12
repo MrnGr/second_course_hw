89d685f3712084164a5ba34a7b99aef75e2fc0b0
marina drozdova <msrinchagrib@mail.ru>
2024-02-12 20:56:12 +0300

    дз3 исправлено

--- hw-3.js
+++ hw-3.js
@@ -26,47 +26,71 @@ let b = '3';
 alert(Number(a) + Number(b));
 
 let monthNumber = 3;
+// switch (monthNumber) {
+//     case 1:
+//         alert('Январь');
+//         break;
+//     case 2:
+//         alert('Февраль');
+//         break;
+//     case 3:
+//         alert('Март');
+//         break;
+//     case 4:
+//         alert('Апрель');
+//         break;
+//     case 5:
+//         alert('Май');
+//         break;
+//     case 6:
+//         alert('Июнь');
+//         break;
+//     case 7:
+//         alert('Июль');
+//         break;
+//     case 8:
+//         alert('Август');
+//         break;
+//     case 9:
+//         alert('Сентябрь');
+//         break;
+//     case 10:
+//         alert('Октябрь');
+//         break;
+//     case 11:
+//         alert('Ноябрь');
+//         break;
+//     case 12:
+//         alert('Декабрь');
+//         break;
+//     default:
+//         alert('Такого месяца нет');
+// }
+
 switch (monthNumber) {
+    case 12:
     case 1:
-        alert('Январь');
-        break;
     case 2:
-        alert('Февраль');
+        alert('Зима');
         break;
     case 3:
-        alert('Март');
-        break;
     case 4:
-        alert('Апрель');
-        break;
     case 5:
-        alert('Май');
+        alert('Весна');
         break;
     case 6:
-        alert('Июнь');
-        break;
     case 7:
-        alert('Июль');
-        break;
     case 8:
-        alert('Август');
+        alert('Лето');
         break;
     case 9:
-        alert('Сентябрь');
-        break;
     case 10:
-        alert('Октябрь');
-        break;
     case 11:
-        alert('Ноябрь');
-        break;
-    case 12:
-        alert('Декабрь');
+        alert('Осень');
         break;
     default:
         alert('Такого месяца нет');
 }
-
 
 input = prompt('Пожалуйста, введите любое число');
 let number = Number(input);
